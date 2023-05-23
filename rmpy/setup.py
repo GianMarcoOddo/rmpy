460eaab2056203322afe8905ff70fa938527cdab
GianMarcoOddo <127972747+GianMarcoOddo@users.noreply.github.com>
2023-05-23 16:35:37 +0200

    Update setup.py

--- rmpy/setup.py
+++ rmpy/setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="rmpy",
-    version="1.2.1",
+    version="1.2.2",
     packages=find_packages(),
     install_requires=[
         'numpy',
